6e39aecbeee0da7aaba8c95a50754b112a710863
Antonio Paneque <agpaneque@gmail.com>
2021-10-11 13:17:37 -0400

    CRUD ok

---
+++ BEComentarios/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BEComentarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BEComentarios
+{
+    public class AplicationDbContext :DbContext
+    {
+        public DbSet<Comentario> Comentario { get; set; }
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
